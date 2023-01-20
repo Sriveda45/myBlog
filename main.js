7534e648c4336c1449c70c9257f0b5660e4760aa
Sriveda45 <105430386+Sriveda45@users.noreply.github.com>
2023-01-19 20:09:08 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var first_name="Sriveda";
+var last_name="Bompalli";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var number_1=5;
+var number_2=10;
+var result=number_1+number_2;
+console.log(result);
